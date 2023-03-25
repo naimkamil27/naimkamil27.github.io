c49be6be10d1ffd8bc81ca759d30ec8de64ea805
General <General@Generals-MacBook-Pro.local>
2023-03-25 14:44:33 +0800

    ty

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-center" style="padding-top: 100px; padding-bottom: 100px;">
         <div style="margin-top: 50px; color: #595959;">
-            <p>Copyright © 2023 NaimKamil. made with <i class='bx bxs-heart'></i> by naimkamil :).</p>
+            <p>Copyright © 2023 NaimKamil. made with <i class='bx bxs-heart'></i> by naimkamil:).</p>
         </div>
     </div>
 </template>
